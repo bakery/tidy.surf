c0db82d7c7f5f9dd4b12a86024e0b009b515270d
Philip <philip.nuzhnyy@gmail.com>
2018-08-04 19:01:49 +0100

    clean up dev script, load env config in dev

--- packages/app/server.js
+++ packages/app/server.js
@@ -2,8 +2,13 @@ import express from 'express';
 import next from 'next';
 import { connect } from './api';
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+if (dev) {
+  require('dotenv').config();
+}
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -11,10 +16,6 @@ app.prepare().then(() => {
   const server = express();
 
   connect(server);
-
-  server.get('/api', (req, res) => {
-    return res.send('>>>>>>>>>>> API Callz, yo');
-  });
 
   server.get('*', (req, res) => {
     return handle(req, res)
