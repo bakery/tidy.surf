65b9ee0a33e7100425ad6283ac21a1ecf5eb7764
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-08-06 15:53:06 +0200

    graphql playground theme improvement

--- packages/app/api/index.js
+++ packages/app/api/index.js
@@ -4,7 +4,16 @@ import { schema, resolvers } from 'tides';
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs: schema, resolvers });
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  playground: {
+    settings: {
+      'editor.theme': 'light',
+      'editor.cursorShape': 'line' // possible values: 'line', 'block', 'underline'
+    },
+  },
+});
 
 export function connect(app) {
   server.applyMiddleware({ app }); 
