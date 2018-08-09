3bda5662a84b487c8044bef4285a44888c9da101
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-08-09 11:42:16 +0100

    yarn locks update

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -9,20 +9,4 @@ module.exports = withCss({
       apiKey: process.env.ALGOLIA_SEARCH_KEY
     }
   },
-  webpack (config) {
-    config.module.rules.push({
-      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          publicPath: './',
-          outputPath: 'static/',
-          name: '[name].[ext]'
-        }
-      }
-    })
-
-    return config
-  }
 })
