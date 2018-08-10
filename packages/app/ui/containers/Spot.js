c8673f9fd885bf03725de225dc60eae9a2eb1deb
Orlando Goncalves <orlando.goncalves@gmail.com>
2018-08-10 18:55:07 +0100

    Query for tides fixed

--- packages/app/ui/containers/Spot.js
+++ packages/app/ui/containers/Spot.js
@@ -4,6 +4,7 @@ import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import Tides from './Tides';
 import Head from 'next/head'
+import { Tab } from 'semantic-ui-react'
 
 export const getSpotById = gql`
   query getSpotById($id: ID!) {
@@ -30,6 +31,12 @@ export default function Spot ({ id }) {
 
         const spot = data && data.spotById;
 
+        const panes = [
+          { menuItem: 'Today', render: () => <Tab.Pane attached='bottom'>Tab 1 Content</Tab.Pane> },
+          { menuItem: 'Tomorrow', render: () => <Tab.Pane attached='bottom'>Tab 2 Content</Tab.Pane> },
+          { menuItem: '10 Days', render: () => <Tab.Pane attached='bottom'>Tab 3 Content</Tab.Pane> },
+        ]
+
         return spot ? (
           <div>
             <Head>
@@ -37,6 +44,7 @@ export default function Spot ({ id }) {
               <meta name="description" content={`${spot.city} tide timetables and charts for the next 10 days`} key="description" />
               <meta name="keywords" content={`${spot.city} tide times,${spot.city} high tide,${spot.city} tide chart,${spot.city} tide tables,${spot.city} low tide`} key="keywords" />
             </Head>
+            <Tab menu={{ attached: 'top' }} panes={panes} />
             <h1>{spot.city}, {spot.state}, {spot.country}</h1>
             <Tides spot={spot} />
           </div>
--- packages/app/ui/containers/Tides.js
+++ packages/app/ui/containers/Tides.js
@@ -4,28 +4,53 @@ import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
 export const getTides = gql`
-  query getTides($lat: Float!, $lon: Float!, $timezone: String!) {
-    tides(lat: $lat, lon: $lon, timezone: $timezone) {
-      dt
-      date
-      height
-      type
+  query($spotId: ID!) {
+    tides(spotId: $spotId){
+      spot {
+        id
+      }
+      currentTime {
+        timezone
+        hours
+        minutes
+        prettyTimeLabel
+      }
+      tides {
+        currentTide {
+          type
+          dt
+        }
+        today {
+          dt
+          date
+          prettyTimeLabel
+          day
+          month
+          year
+          height
+          type
+        }
+      }
     }
   }
 `;
 
 export default function Tides ({ spot }) {
-  const { lat, lon, timezone } = spot;
+  console.log('spot', spot);
+  const { citySlug, stateSlug, countrySlug } = spot;
   return (
-    <Query query={getTides} variables={{ lat, lon, timezone }}>
+    <Query query={getTides} variables={{ spotId: `${citySlug}-${stateSlug}-${countrySlug}` }}>
       {({ loading, error, data }) => {
+        console.log('error', error);
         if (loading) return "Loading...";
         if (error) return `Error! ${error.message}`;
+
+        console.log('data', data);
 
         return (
           <section>
             <ul>
-              {data.tides.map(({ dt, date, height, type }) => (
+              {data.tides.tides.today.map(({ dt, date, height, type }) => (
                 <li key={dt}>
                   ({type}) - {height}m @ {date}
                 </li>
