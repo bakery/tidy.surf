ee06f434f799ed71c62bb90760d7731dacede8e5
Philip <philip.nuzhnyy@gmail.com>
2018-08-21 17:59:40 +0100

    wrap up

--- data/index.js
+++ data/index.js
@@ -2,57 +2,72 @@ require('dotenv').config();
 
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 const mongoose = require('mongoose');
-const request = require('request');
-const { Location } = require('./models/location');
-const { geocodeLocation, getTimezone } = require('./geo');
+const { Location, attachGeo, markLocationAsChecked } = require('./models/location');
+const { getGeoAndTZ } = require('./geo');
 
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-// const db = mongoose.connection;
-
-// db.on('error', error => console.error(`Failed to connect to the DB: ${error.message}`));
-// db.once('open', () => {
-//   console.log('All connected!');
-
-//   const baseDir = './results';
-
-//   fs.readdir(baseDir, (err, files) => {
-//     if (err) {
-//       console.error(`Failed to grab files from ./results: ${err.message}`);
-//       return;
-//     }
-
-//     files.forEach( function(file) {
-//       const fullPath = path.resolve(baseDir, file);
-//       const locations = JSON.parse(fs.readFileSync(fullPath));
-
-//       console.log(`processing ${fullPath} - ${locations.length} locations`);
-
-//       Location.insertMany(locations, (error, docs) => {
-//         if (error) {
-//           console.error(`Failed to insert locations for ${file}: ${error.message}`);
-//         } else {
-//           console.log(`Locations inserted for ${file}`);
-//         }
-//       });
-//     });
-//   });
-// });
-
-geocodeLocation('Japan', [
-  "Yasugi",
-  "Simane",
-  "Japan"
-]).then(location => {
-  console.log('got geo', location)
-  getTimezone(location.lat, location.lng).then(tz => {
-    console.log('Got tz', tz)
-  })
-}).catch(error => {
-  console.error('geo fuck', error)
-})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
 
 
+const geoTagEverything = () => {
+  Location.findOne().needsLocationAndTimezoneInfo().exec((err, item) => {
+    if (err) {
+      console.error(`Failed to grab item to update: ${err.message}`);
+      geoTagEverything();
+    } else {
+      if (!item) {
+        return;
+      }
+
+      console.log('Gonna geo tag', item);
+
+      const { country, locationParts } = item;
+
+      getGeoAndTZ(country, locationParts).then(locationInfo => {
+        console.log('location info is:', JSON.stringify(locationInfo));
+        return attachGeo(item, locationInfo);
+      }).catch(error => {
+        console.error(error.message);
+        markLocationAsChecked(item);
+      }).then(() => {
+        geoTagEverything();
+      });
+    }
+  });  
+};
+
+const loadJSONIntoDB = () => {
+  const baseDir = './results';
+
+  fs.readdir(baseDir, (err, files) => {
+    if (err) {
+      console.error(`Failed to grab files from ./results: ${err.message}`);
+      return;
+    }
+
+    files.forEach( function(file) {
+      const fullPath = path.resolve(baseDir, file);
+      const locations = JSON.parse(fs.readFileSync(fullPath));
+
+      console.log(`processing ${fullPath} - ${locations.length} locations`);
+
+      Location.insertMany(_.map(locations, l => _.extend(l, { city: l.name })), (error, docs) => {
+        if (error) {
+          console.error(`Failed to insert locations for ${file}: ${error.message}`);
+        } else {
+          console.log(`Locations inserted for ${file}`);
+        }
+      });
+    });
+  });
+}
+
+db.on('error', error => console.error(`Failed to connect to the DB: ${error.message}`));
+db.once('open', () => {
+  console.log('All connected!');
+});
 process.stdin.setRawMode(true);
 process.stdin.resume();
 process.stdin.on('data', process.exit.bind(process, 0));
--- data/models/location.js
+++ data/models/location.js
@@ -5,14 +5,53 @@ const locationSchema = new Schema({
   city: String,
   country: String,
   countryCode: String,
+  region: String,
   timezone: String,
   locationParts: [String],
   lat: Number,
   lng: Number,
+  lastChecked: Date
 });
+
+locationSchema.query.needsLocationAndTimezoneInfo = function(name) {
+  return this.where({
+    lat: {
+      $exists: false,
+    },
+    lng: {
+      $exists: false,
+    },
+    timezone: {
+      $exists: false,
+    },
+    lastChecked: {
+      $exists: false,
+    },
+  });
+};
 
 const Location = mongoose.model('Location', locationSchema);
 
+const markLocationAsChecked = (location) => {
+  return Location.updateOne({ _id: location._id }, {
+    $set: {
+      lastChecked: new Date()
+    }
+  }).exec();
+};
+
 module.exports = {
-  Location
+  Location,
+  markLocationAsChecked,
+  attachGeo(location, { lat, lng, region, timezone }) {
+    return Location.updateOne({ _id: location._id }, {
+      $set: {
+        lat,
+        lng,
+        timezone,
+        region,
+        lastChecked: new Date()
+      }
+    }).exec();
+  }
 }
